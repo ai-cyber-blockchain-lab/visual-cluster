5bd74466861cef37c4841cf4be64cbc6e3b63718
Martin Wilz <github@wilz.de>
2018-07-14 14:45:29 +0200

    first step: draw a blue circle

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main(void) {
-    printf("Hello World\n");
+int main()
+{
+    int screenWidth = 1280;
+    int screenHeight = 1024;
+
+    InitWindow(screenWidth, screenHeight, "particles");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        // TODO: Update your variables here
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawCircle(screenWidth/2, screenHeight / 2, screenHeight / 2, DARKBLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
